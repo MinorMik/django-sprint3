9388608e440f1cf0625da934bf375cf1a09cbaad
Геннадий Плыс <gplys@mail.ru>
2026-06-03 20:53:19 +0300

    первое ревью

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from blog.models import Post, Category
 from django.utils import timezone
 
@@ -28,8 +29,10 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_id):
     category = get_object_or_404(Category, slug=category_id)
+    if not category.is_published:
+        raise Http404("Категория снята с публикации")
 
-    posts = Post.objects.filter(
+    post_list = Post.objects.filter(
         category=category,
         is_published=True,
         pub_date__lte=timezone.now()
@@ -37,6 +40,6 @@ def category_posts(request, category_id):
 
     context = {
         'category': category,
-        'posts': posts
+        'post_list': post_list
     }
     return render(request, 'blog/category.html', context)
